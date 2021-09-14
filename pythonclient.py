3161b927bd367e5bc06c0ef03d59e80559ad653b
Abhraneel <84504774+GunPowderAb@users.noreply.github.com>
2021-09-14 23:45:29 +0530

    Update pythonclient.py

--- pythonclient.py
+++ pythonclient.py
@@ -3,6 +3,14 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 50007))
 client.send("It's your client. Hi".encode())
-
+#receive values from server
 from_server = client.recv(4096)
-print(from_server)
+print(from_server.decode())
+print("Type what you want to send to server")
+while True:
+    s=input()
+    client.send(s.encode())
+    #close the program if you type 'exit'
+    if s=='exit':
+        client.close()
+        exit()
